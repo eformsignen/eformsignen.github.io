d6a23045ade6df910aa7b1aad5959b2dcced15ef
davidchoi <davidchoi@forcs.com>
2023-01-05 18:16:17 +0900

    changed theme to book

--- conf.py
+++ conf.py
@@ -38,13 +38,11 @@ release = '2021.03.31'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
 
 extensions = [
-    'sphinx_rtd_theme',
+
 ]
 
-html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
